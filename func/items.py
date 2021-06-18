b5175440f34a305a003f322fcf8d760409d0896c
Marseel-E <74118311+Marseel-E@users.noreply.github.com>
2021-06-18 18:23:53 -0400

    Update items.py

--- func/items.py
+++ func/items.py
@@ -85,17 +85,9 @@ items = {
             "icon": ":brown_circle:",
             "price": 1000,
         },
-        "kek": {
-            "icon": "<:kek:848798371170222121>",
-            "price": 20000,
-        },
         "wanky": {
             "icon": "<:wanky:848829643947507763>",
             "price": 600009,
-        },
-        "aidas": {
-            "icon": "<:aidas:848829654591733830>",
-            "price": 1000000,
         },
     },
 
