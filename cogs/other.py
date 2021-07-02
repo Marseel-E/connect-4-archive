c05162d6f4bc48d8a8e815533faf1ab963fc0792
Marseel-E <74118311+Marseel-E@users.noreply.github.com>
2021-07-02 03:56:17 -0400

    Add files via upload

--- cogs/other.py
+++ cogs/other.py
@@ -4,6 +4,7 @@ from func import default
 from func.human import *
 from func import database as db
 from datetime import datetime, timedelta
+from func.default import limit
 
 
 class Other(commands.Cog):
@@ -32,10 +33,186 @@ class Other(commands.Cog):
         await ctx.message.delete()
     
 
-    @commands.command(help="An advanced embed creation command.\n\n*Use quotations (`' '`) for sentences.\n*Use `None` for an unwanted argument.\n__Example__:\n```\nembed None 'amazing description'\n```That would create an embed with only description.\n\n*Use brackets (`[ ]`) to hold your fields.\n*Field name, value & inline (`True`,`False`) should be seperated by (`\s `) inside your string/quotation, inside the bracket.\n__Example__:\n```\nembed ['field_1\s field_2\s True', 'field_2\s value_2\s True', 'field_3\s value_3\s False']\n```That would create an embed with only 3 fields: if field 1: if 2 will be in the same line but field 3 will be in a seperate line.", aliases=['e'])
-    async def embed(self, ctx, title : typing.Optional[str] = None, description : typing.Optional[str] = None, color : typing.Optional[str] = "5261f8", author : typing.Optional[discord.Member] = None, thumbnail : typing.Optional[str] = None, image : typing.Optional[str] = None, footer : typing.Optional[str] = None, fields : typing.Optional[list] = None):
-        await default.Embed.maintenance(ctx); return
-        await ctx.send(embed=embed)
+    @commands.command(help="Advanced embed creation command.", aliases=['create-embed', 'e'])
+    async def embed(self, ctx):
+        TITLE = None; DESCRIPTION = None; COLOR = '000000'; AUTHOR = None; FOOTER = None; IMAGE = None; THUMBNAIL = None; FIELDS = None
+        
+        tmt = 120
+
+        async def TIMEOUT():
+            await message.delete()
+            embed = default.Embed.error('MessageTimedOut', f"{ctx.author.mention}, You failed to respond in-time.")
+            await ctx.send(embed=embed, delete_after=15)
+        
+        async def LIMIT(limit : int):
+            await message.delete()
+            embed = default.Embed.error('LimitReached', f"You've reached the maximum character limit. ([{len(message.content)}]({message.jump_url})/{HL(limit)})")
+            await ctx.send(embed=embed, delete_after=15)
+        
+        async def LINK():
+            await message.delete()
+            embed = default.Embed.error('ValueError', f"This value should be a link type. \n({HL('https://imgur.com/gallery/q261Q')})")
+            await ctx.send(embed=embed, delete_after=15)
+        
+        def RULE(limit : typing.Optional[int] = None):
+            text = f"Respond with {HL('no')} if you don't want this value."
+            if (limit): text = f"Respond with {HL('no')} if you don't want this value.\n (Limit: {HL(limit)})"
+            return text
+        
+        embed = default.Embed.minimal("RULES", f"-Embed sum must not exceed 6000!\n-Each embed value has its own limit!\n-Respond with a {HL('no')} if you don't want a specific value.", default.Color.blurple)
+        msg = await ctx.send(embed=embed)
+        await asyncio.sleep(3.0)
+
+        def check(m):
+            return m.channel.id == ctx.channel.id and m.author.id == ctx.author.id
+
+        # Title, Description, Color
+
+        #- Title
+        embed = default.Embed.minimal(f"What would you like your {HL('title')} to be ?", RULE(limit['title']), default.Color.dark_grey)
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if len(message.content) > limit['title']: await LIMIT(limit['title']); continue
+                    TITLE = message.content; await message.delete(); break
+
+        #- Description
+        embed = default.Embed.minimal(None, f"What would you like your {HL('description')} to be ?\n{RULE(limit['description'])}", default.Color.dark_grey)
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if len(message.content) > limit['description']: await LIMIT(limit['description']); continue
+                    DESCRIPTION = message.content; await message.delete(); break
+
+        #- Color
+        embed = default.Embed.minimal(f"What would you like your {HL('color')} to be ?", f"{HL(default.Color.blurple)}\n{RULE()}", default.Color.blurple)
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                if int(message.content, 16) > 16777215: await message.delete(); await ctx.send("Please use a valid HEX color code"); continue
+                else: COLOR = message.content; await message.delete(); break
+
+        # Author, Footer
+
+        #- Author
+        embed = default.Embed.custom(f"Who would you like your {HL('author')} to be ?", RULE(limit['author']), default.Color.dark_grey, None, ctx.author)
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if len(message.content) > limit['author']: await LIMIT(limit['author']); continue
+                    AUTHOR = message.mentions[0]; await message.delete(); break
+        
+        #- Footer
+        embed = default.Embed.custom(None, RULE(limit['footer']), default.Color.dark_grey, None, None, f"What would you like your footer to be ?")
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if len(message.content) > limit['footer']: await LIMIT(limit['footer']); continue
+                    FOOTER = message.content; await message.delete(); break
+
+        # Image, Thumbnail
+        
+        #- Image
+        embed = default.Embed.custom(f"What would you like your image to be ?", RULE(), default.Color.dark_grey, None, None, None, 'https://imgur.com/gallery/q261Q')
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if not message.content.startswith('https://'): await LINK(); continue
+                    IMAGE = message.content; await message.delete(); break
+        
+        #- Thumbnail
+        embed = default.Embed.custom(f"What would you like the thumbnail to be ?", RULE(), default.Color.dark_grey, None, None, None, None, 'https://imgur.com/gallery/q261Q')
+        await msg.edit(embed=embed)
+
+        while True:
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                else:
+                    if not message.content.startswith('https://'): await LINK(); continue
+                    THUMBNAIL = message.content; await message.delete(); break
+
+        # Fields
+        while True:
+            embed = default.Embed.custom(None, None, default.Color.dark_grey, ['Would you like to\s add a field ?\s True'], None, "Yes / No")
+            await msg.edit(embed=embed)
+
+            try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+            except asyncio.TimeoutError: await TIMEOUT(); break
+            else:
+                if message.content.lower() == "no": await message.delete(); break
+                if message.content.lower() == "yes":
+                    if FIELDS == None: FIELDS = []
+                    await message.delete()
+
+                    embed = default.Embed.custom(f"What would you like the {HL('name')} to be?", None, default.Color.dark_grey, ['Name\s Value\s True'])
+                    await msg.edit(embed=embed)
+                    
+                    while True:
+                        try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+                        except asyncio.TimeoutError: await TIMEOUT(); break
+                        else:
+                            if len(message.content) > limit['field']['name']: await LIMIT(limit['field']['name']); continue
+                            FIELD_NAME = message.content; await message.delete(); break
+                    
+                    embed = default.Embed.custom(f"What would you like the {HL('value')} to be?", None, default.Color.dark_grey, ['Name\s Value\s True'])
+                    await msg.edit(embed=embed)
+                    
+                    while True:
+                        try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+                        except asyncio.TimeoutError: await TIMEOUT(); break
+                        else:
+                            if len(message.content) > limit['field']['value']: await LIMIT(limit['field']['value']); continue
+                            FIELD_VALUE = message.content; await message.delete(); break
+                    
+                    embed = default.Embed.custom(f"Would you like the field to be {HL('inline')} with the previous/next field?", None, default.Color.dark_grey, None, None, "Yes/No")
+                    await msg.edit(embed=embed)
+                    
+                    while True:
+                        try: message = await self.client.wait_for('message', check=check, timeout=tmt)
+                        except asyncio.TimeoutError: await TIMEOUT(); break
+                        else:
+                            if message.content.lower() == "no": FIELD_INLINE = False; await message.delete(); break
+                            if message.content.lower() == "yes": FIELD_INLINE = True; await message.delete(); break
+                
+                FIELDS.append(f"{FIELD_NAME}\s {FIELD_VALUE}\s {FIELD_INLINE}")
+
+        embed = default.Embed.success(None, "Preparing embed...")
+        await msg.edit(embed=embed)
+
+        # Send
+        embed = default.Embed.custom(TITLE, DESCRIPTION, COLOR, FIELDS, AUTHOR, FOOTER, IMAGE, THUMBNAIL)
+        await msg.delete(); await ctx.send(embed=embed)
 
 
     @commands.group(invoke_without_command=True, help="Shows the bot's stats.")
